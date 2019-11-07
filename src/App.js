44b802c2bdecb6b57558225115ee3e64a84029cb
Thiago Marinho <tgmarinho@gmail.com>
2019-11-07 12:31:59 -0400

    Relay Step by Step - fetching data in the Github API GraphQL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import fetchGraphQL from "./fetchGraphQL";
+
+const { useState, useEffect } = React;
 
 function App() {
+  // We'll load the name of a repository, initially setting it to null
+  const [name, setName] = useState(null);
+
+  // When the component mounts we'll fetch a repository name
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        # feel free to change owner/name here
+        repository(owner: "tgmarinho" name: "meetapp") {
+          name
+        }
+      }
+    `)
+      .then(response => {
+        // Avoid updating state if the component unmounted before the fetch completes
+        if (!isMounted) {
+          return;
+        }
+        const data = response.data;
+        setName(data.repository.name);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  // Render "Loading" until the query completes
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{name != null ? `Repository: ${name}` : "Loading"}</p>
       </header>
     </div>
   );
